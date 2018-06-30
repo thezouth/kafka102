83962a805129e8fe6fd9c4f08d436d204584375c
tansinee <yimlaguna@gmail.com>
2018-06-30 11:13:26 +0700

    Update kafka-install.sh

--- kafka-install.sh
+++ kafka-install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Download and unarchive Kafka.
-curl -O http://www-eu.apache.org/dist/kafka/1.1.0/kafka_2.11-1.1.0.tgz   
+curl -O http://www-eu.apache.org/dist/kafka/1.1.0/kafka_2.11-1.1.0.tgz
 
-tar -xvf kafka-1.1.0.tgz
-cd kafka-1.1.0
+tar -xvf kafka_2.11-1.1.0.tgz
+cd kafka_2.11-1.1.0
 
 # Start Kafka
 ./bin/kafka-server-start.sh conf/server.properties
